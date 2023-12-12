2a03a78b688ace1a07a82c012150320c79a1b29e
wahyudi prasatia <wahyudiprasatia3@gmail.com>
2023-12-12 19:50:12 +0700

    add camera

--- esp32cam/esp32cam.ino
+++ esp32cam/esp32cam.ino
@@ -1,12 +1,40 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include "Base64.h"
 
 // Update these with values suitable for your network.
 
 const char* ssid = "KOWI";
 const char* password = "kremi201";
 const char* mqtt_server = "103.84.207.210";
+
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long interval = 150;
+
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define MQTT_PUBLISH_CAM_TOPIC "esp32/cam_0"
 
 
 // Pin untuk mengontrol motor kiri
@@ -15,7 +43,7 @@ int in1 = 13;
 int in2 = 15;
 
 // Pin untuk mengontrol motor kanan
-int enB = 0;  // Pin PWM
+int enB = 12;  // Pin PWM
 int in3 = 14;
 int in4 = 2;
 
@@ -27,7 +55,6 @@ void maju(int kecepatan) {
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   ledcWrite(6, kecepatan);  // PWM untuk motor kiri
-  ledcWrite(7, kecepatan);  // PWM untuk motor kanan
 }
 
 void mundur(int kecepatan) {
@@ -36,7 +63,6 @@ void mundur(int kecepatan) {
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   ledcWrite(6, kecepatan);  // PWM untuk motor kiri
-  ledcWrite(7, kecepatan);  // PWM untuk motor kanan
 }
 
 void belokKanan(int kecepatan) {
@@ -45,7 +71,6 @@ void belokKanan(int kecepatan) {
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   ledcWrite(6, kecepatan - 25);  // PWM untuk motor kiri
-  ledcWrite(7, kecepatan - 25 );  // PWM untuk motor kanan
 }
 
 void belokKiri(int kecepatan) {
@@ -54,7 +79,6 @@ void belokKiri(int kecepatan) {
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   ledcWrite(6, kecepatan -25 );  // PWM untuk motor kiri
-  ledcWrite(7, kecepatan -25 );  // PWM untuk motor kanan
 }
 
 void stop() {
@@ -63,7 +87,6 @@ void stop() {
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   ledcWrite(6, 0);  // PWM untuk motor kiri
-  ledcWrite(7, 0);  // PWM untuk motor kanan
 }
 
 
@@ -177,12 +200,133 @@ void reconnect() {
   }
 }
 
+
+
+String sendImage() {
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return "Camera capture failed";
+  }
+
+  char *input = (char *)fb->buf;
+  char output[base64_enc_len(3)];
+  String imageFile = "data:image/jpeg;base64,";
+  for (int i = 0; i < fb->len; i++) {
+    base64_encode(output, (input++), 3);
+    if (i % 3 == 0) imageFile += String(output);
+  }
+  int fbLen = imageFile.length();
+
+  String clientId = "ESP32-";
+  clientId += String(random(0xffff), HEX);
+  if (client.connected()) {
+    //https://github.com/knolleary/pubsubclient/blob/master/src/PubSubClient.h
+
+    client.beginPublish(MQTT_PUBLISH_CAM_TOPIC, fbLen, true);
+
+    String str = "";
+    for (size_t n = 0; n < fbLen; n = n + 2048) {
+      if (n + 2048 < fbLen) {
+        str = imageFile.substring(n, n + 2048);
+        client.write((uint8_t*)str.c_str(), 2048);
+      }
+      else if (fbLen % 2048 > 0) {
+        size_t remainder = fbLen % 2048;
+        str = imageFile.substring(n, n + remainder);
+        client.write((uint8_t*)str.c_str(), remainder);
+      }
+    }
+
+    client.endPublish();
+
+    esp_camera_fb_return(fb);
+
+    return "";
+  }
+  esp_camera_fb_return(fb);
+  return "failed, rc=" + client.state();
+}
+
+
+void initCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  //
+  // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+  //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+  //            Partial images will be transmitted if image exceeds buffer size
+  //
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (psramFound()) { //是否有PSRAM(Psuedo SRAM)記憶體IC
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  //視訊初始化
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+
+  //可自訂視訊框架預設大小(解析度大小)
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);    //解析度 SVGA(800x600), VGA(640x480), CIF(400x296), QVGA(320x240), HQVGA(240x176), QQVGA(160x120), QXGA(2048x1564 for OV3660)
+
+  //s->set_vflip(s, 1);  //垂直翻轉
+  //s->set_hmirror(s, 1);  //水平鏡像
+
+  //閃光燈(GPIO4)
+  //ledcAttachPin(4, 4);
+  //ledcSetup(4, 5000, 8);
+}
+
 void setup() {
+
   Serial.begin(115200);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   setup_wifi();
   delay(500);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  client.setBufferSize(2048);
+
+  initCamera();
 
 //  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   pinMode(in1, OUTPUT);
@@ -193,9 +337,7 @@ void setup() {
 
   // Konfigurasi PWM untuk motor kiri dan kanan
   ledcSetup(6, 5000, 8);  // Kanal 0, frekuensi 5000Hz, resolusi 8-bit
-  ledcSetup(7, 5000, 8);  // Kanal 1, frekuensi 5000Hz, resolusi 8-bit
   ledcAttachPin(enA, 6);  // Hubungkan PWM ke pin enA
-  ledcAttachPin(enB, 7);  // Hubungkan PWM ke pin enB
 
   stop();
 
@@ -205,6 +347,14 @@ void loop() {
 
   if (!client.connected()) {
     reconnect();
+  }
+  else {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+      sendImage();
+    }
   }
   client.loop();
 }
