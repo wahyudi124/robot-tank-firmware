4ae9b25f94fdf5d86b1ce3afce560a9948d4a59c
wahyudi prasatia <wahyudiprasatia3@gmail.com>
2023-12-16 20:21:54 +0700

    add oled

--- esp32controller/esp32controller.ino
+++ esp32controller/esp32controller.ino
@@ -4,6 +4,17 @@
 #include "DHT.h"
 #include <MQUnifiedsensor.h>
 #include <ESP32Servo.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
 Servo pan;
 Servo tilt;
 
@@ -349,6 +360,21 @@ void setup() {
     while (1)
       ;
   }
+
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Robot Tank");
+  display.display(); 
 }
 
 void loop() {
@@ -385,6 +411,12 @@ void loop() {
       serializeJson(doc, jsonStr);
 
       client.publish("/sensor", jsonStr.c_str());
+      display.setCursor(0, 0);
+      display.println("Temperature: " + String(t));
+      display.setCursor(0, 1);
+      display.println("Gass: " + String(gass));
+
+      display.display();
 
       previousMillis = currentMillis;
     }
